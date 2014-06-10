bb54189a6de71bd49acee187cf6cf91735755207
Timur <timursayfullin@gmail.com>
2014-06-10 23:14:50 +0300

    angular sample

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,41 @@
 
 angular.module('testAngularJsApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.event = {
+        name: 'Angular Boot Camp',
+        date: '1/1/2013',
+        time: '10.30 pm',
+        location:{
+            address: 'Google Headqoters',
+            city:'Mountain View',
+            province: 'CA'
+        },
+        imageUrl: '',
+        sessions:[
+            {
+                name: 'Directives Masterclass',
+                creatorName:'Bob Smith',
+                duration:'1 hr',
+                level:'Advanced',
+                abstract:'In this session you will learn the ins and cuts of directives',
+                upVoteCount: 0
+            },
+            {
+                name: 'Scopes for fun and profit',
+                creatorName:'John Doe',
+                duration:'30 min',
+                level:'Introductory',
+                abstract:'This session will take a closer look at scopes.',
+                upVoteCount: 0
+            },
+            {
+                name: 'Well behaved controllers',
+                creatorName:'Jane Dou',
+                duration:'2 hours',
+                level:'Intermediate',
+                abstract:'Controllers are the beginning of everything Angular does ',
+                upVoteCount: 0
+            }
+        ]
+    };
   });
